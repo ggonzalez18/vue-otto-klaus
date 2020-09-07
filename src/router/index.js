cefffc578f20efedc4336470f6d6ca0b28e630d7
Gabriela Gonzalez <gabytasmile7@gmail.com>
2020-09-07 18:43:28 -0400

    se inicio proyecto, se instalo vuetify y se enruto

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import toys from '../views/toys.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +12,15 @@ Vue.use(VueRouter)
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/toys',
+    name: 'Toys',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "toys" */ '../views/Toys.vue'),
+    meta: {
+      login: true //login es un nombre X puede ser cualquiera referente a la autenticacion
+    }
   }
 ]
 
